ea03797e781341bd0eafa5ac531f6769ec065d18
Jason Dagit <dagit@galois.com>
2011-06-04 14:58:19 -0700

    Add a slightly more interesting example defining map

--- examples/Haskell_Map.hs
+++ examples/Haskell_Map.hs
@@ -8,9 +8,15 @@ map :: (a -> b) -> List a -> List b
 map f Nil         = Nil
 map f (Cons x xs) = Cons (f x) (map f xs)
 
-
+{-
 class Functor f where
   fmap :: (a -> b) -> f a -> f b
 
 instance Functor List where
   fmap = map
+-}
+{-
+data Bool = True | False
+newtype NotBool = Ninja Bool
+notTrue = Ninja True
+-}
