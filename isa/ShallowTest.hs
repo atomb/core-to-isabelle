415a5a459233c00a8111ad4a72afe2429c122f68
Jason Dagit <dagit@galois.com>
2011-06-04 14:57:45 -0700

    Checkin rewrite of translator

--- isa/ShallowTest.hs
+++ isa/ShallowTest.hs
@@ -1,25 +1,26 @@
-import Language.Core.Core
+import Language.Core.Syntax
 import Language.Core.Parser
-import Language.Core.ParseGlue
+-- import Language.Core.ParseGlue
 import System.Environment
 import Data.List ( intercalate, isSuffixOf )
 import System.FilePath ( takeBaseName )
+import qualified Data.ByteString.Lazy.Char8 as L
 
 processTdef :: Tdef -> String
-processTdef (Data (_, name) tbinds cdefs) =
-  name ++ " " ++
+processTdef (Data (_, _, name) tbinds cdefs) =
+  L.unpack name ++ " " ++
   intercalate " " (map processTbindQuoted tbinds) ++
   " = " ++ intercalate " | " (map processCdef cdefs)
 processTdef (Newtype {}) = error "newtype not yet implemented"
   
 processTbindQuoted :: Tbind -> String
-processTbindQuoted (tvar, Klifted)       = tvar
-processTbindQuoted (tvar, k@(Karrow {})) = "(" ++ tvar ++ " :: " ++
+processTbindQuoted (tvar, Klifted)       = L.unpack tvar
+processTbindQuoted (tvar, k@(Karrow {})) = "(" ++ L.unpack tvar ++ " :: " ++
   quote (showKind k) ++ ")"
 
 processTbind :: Tbind -> String
-processTbind (tvar, Klifted)       = tvar
-processTbind (tvar, k@(Karrow {})) = "(" ++ tvar ++ " :: " ++ showKind k ++ ")"
+processTbind (tvar, Klifted)       = L.unpack tvar
+processTbind (tvar, k@(Karrow {})) = "(" ++ L.unpack tvar ++ " :: " ++ showKind k ++ ")"
 
 showKind :: Kind -> String
 showKind Klifted        = "\\<star>"
@@ -29,50 +30,55 @@ showKind (Karrow k1 k2) = showKind k1 ++ " \\<rightarrow> " ++ showKind k2
 showKind (Keq _ _)      = error "equality kinds not supported"
 
 processCdef :: Cdef -> String
-processCdef (Constr (_, name) [] [])  = name
-processCdef (Constr (_, name) [] tys) = name ++ " " ++
+processCdef (Constr (_, _, name) [] [])  = L.unpack name
+processCdef (Constr (_, _, name) [] tys) = L.unpack name ++ " " ++
   intercalate " " (map (quote.showTy) tys)
 
 showTy :: Ty -> String
-showTy (Tvar v)      = v
+showTy (Tvar v)      = L.unpack v
 showTy (Tapp t1 t2)  = "(" ++ showTy t1 ++ " " ++ showTy t2 ++ ")"
-showTy (Tcon (_, t))
-  | "ZLzmzgZR" `isSuffixOf` t = "funT"
-  | otherwise = t
+showTy (Tarrow t1 t2) = "(" ++ "funT " ++ showTy t1 ++ " " ++ showTy t2 ++ ")"
+showTy (Tcon (_, _, t))
+  | "ZLzmzgZR" `isSuffixOf` L.unpack t = "funT"
+  | otherwise = L.unpack t
 showTy (Tforall tbind ty)  = "(forall " ++ processTbind tbind ++
   ". " ++ showTy ty ++ ")"
-showTy t             = error $ "Ty not full implemented: " ++ show t
+showTy t             = error $ "Ty not fully implemented: " ++ show t
 
 quote :: String -> String
 quote s = "\"" ++ s ++ "\""
 
 processVdef :: Vdef -> String
-processVdef (Vdef ((_, name), ty, exp)) = name ++ " :: " ++
+processVdef (Vdef _ (_, _, name) ty exp) = L.unpack name ++ " :: " ++
   quote (showTy ty) ++ " =\n    " ++ quote (processExp exp)
 
 lambda :: String
 lambda = "\\<lambda>"
 
 processExp :: Exp -> String
-processExp (Var  (_,v))     = v
-processExp (Dcon (_, d))    = d
+processExp (Var  (_, _,v))     = L.unpack v
+processExp (Dcon (_, _, d))    = L.unpack d
 processExp (Lit  {})        = error "Embedding literals not yet supported."
 processExp (App  exp1 exp2) = "(" ++ processExp exp1 ++ " " ++ processExp exp2 ++ ")"
 processExp (Appt exp ty)    = "(" ++ processExp exp ++ " @" ++ showTy ty ++ ")"
-processExp (Lam bind exp)   = lambda ++ " " ++ processBind bind ++ ".\n     " ++ processExp exp
+processExp (Lam bind exp)   = lambda ++ " " ++ processVbind bind ++ ".\n     " ++ processExp exp
+processExp (Lamt bind exp)  = lambda ++ " @" ++ processTbind bind ++ ".\n     " ++ processExp exp
 processExp (Case exp vbind ty alts) =
   "case " ++ "(" ++ showTy ty ++ ") " ++
-  processExp exp ++ " of " ++ fst vbind ++ "\n      " ++
+  processExp exp ++ " of " ++ unVbind vbind ++ "\n      " ++
   " { " ++ intercalate ";\n         " (map processAlt alts) ++ "\n       }"
 processExp exp              = error $ "Not implemented: " ++ show exp
 -- TODO: Finish this definition
+
+unVbind :: Vbind -> String
+unVbind ((_, _, v), _) = L.unpack v
 
 rightArrow :: String
 rightArrow = "\\<rightarrow>"
 
 processAlt :: Alt -> String
-processAlt (Acon (_, dcon) [] vbinds exp) =
-  dcon ++ " " ++ intercalate " " (map (processBind . Vb) vbinds) ++ " " ++
+processAlt (Acon (_, _, dcon) [] vbinds exp) =
+  L.unpack dcon ++ " " ++ intercalate " " (map processVbind vbinds) ++ " " ++
   rightArrow ++ " " ++ processExp exp
 processAlt (Adefault exp) = "_ " ++ rightArrow ++ " " ++ processExp exp
 processAlt alt = error $ "Alt not implemented: " ++ show alt
@@ -85,12 +91,12 @@ Alit Lit Exp
 Adefault Exp	 
 -}
 
-processBind :: Bind -> String
-processBind (Vb vbind) = "(" ++ processVbind vbind  ++ ")"
-processBind (Tb tbind) = " @" ++ processTbind tbind
+-- processBind :: Bind -> String
+-- processBind (Vb vbind) = "(" ++ processVbind vbind  ++ ")"
+-- processBind (Tb tbind) = " @" ++ processTbind tbind
 
 processVbind :: Vbind -> String
-processVbind (var, ty) = var ++ "::" ++ showTy ty
+processVbind ((_,_,var), ty) = "(" ++ (L.unpack var ++ "::" ++ showTy ty) ++ ")"
 
 {-  The goal is to get something of the form:
   halicore_fun
@@ -99,21 +105,22 @@ processVbind (var, ty) = var ++ "::" ++ showTy ty
 See Halicore_Syntax.thy for examples
 -}
 
+{-
 convert :: Exp -> Term
 convert (Var (_,v)) = TmVar v
 convert (Dcon (_,d)) = VCon d 
-convert (Lit (Literal (Lint i) _)) = VInt i
+-- convert (Lit (Literal (Lint i) _)) = VInt i
 convert (Lit _) = undefined {- Rationals, Strings, Chars? -}
 convert (App e e') = VApp (convert e) (convert e')
 convert (Appt e t') = TApp (convert e) t'
-convert (Lam (Vb (v, ty)) e) = VLam v ty (convert e)
-convert (Lam (Tb (v, k)) e) = TLam v (convert e)
+-- convert (Lam (Vb (v, ty)) e) = VLam v ty (convert e)
+-- convert (Lam (Tb (v, k)) e) = TLam v (convert e)
 convert (Let v e) = undefined
 convert (Case e v _ alts) = undefined
 convert (Cast e _) = convert e
 convert (Note _ e) = convert e
 convert (External _ _) = undefined
-
+-}
 processVdefg :: Vdefg -> String
 processVdefg (Nonrec vdef) = halicore_fun ++ processVdef vdef
 processVdefg (Rec vdefs) = halicore_fun ++
@@ -133,6 +140,7 @@ processTdefs [] = []
 processTdefs tdefs = "halicore_data\n  " ++
   intercalate "\nand\n  " (map processTdef tdefs) ++ "\n"
 
+{-
 data IsaDefs = Defs [TyDecl] [TmDecl]
 
 data TyDecl = TyDecl
@@ -140,11 +148,11 @@ data TmDecl = TmDecl IId Term
 
 type IId = String
 data ITy = ITyvar IId
-
+-}
 header :: FilePath -> String
 header name = unlines ["theory " ++ name,
                   "imports Halicore\nbegin"]
-
+{-
 embedTy :: Ty -> String
 embedTy (Tvar v)            = v
 embedTy (Tcon (_, v))       = v -- FIXME: Do not drop the qualifier
@@ -157,7 +165,8 @@ embedTy (UnsafeCoercion {}) = error "UnsafeCoercion"
 embedTy (InstCoercion   {}) = error "InstCoercion"
 embedTy (LeftCoercion   {}) = error "LeftCoercion"
 embedTy (RightCoercion  {}) = error "RightCoercion"
-
+-}
+{-
 embed :: Term -> String
 embed (VInt i) = "VInt (Def "++(show i)++")"
 embed (VApp t t') = "V_app "++(embed t)++ " " ++ (embed t')
@@ -169,15 +178,16 @@ embed (VCon i) = "Vcon\\<cdot>(Def "++ i++")"
 
 embedM :: IsaDefs -> String
 embedM (Defs _ tms) = unlines $ map embedTmDecl tms
-
+-}
 nameConv :: String -> String
 nameConv = ('h':)
-
+{-
 embedTmDecl :: TmDecl -> String
 embedTmDecl (TmDecl i t) = 
   unlines ["definition " ++ (nameConv i) ++ " where",
            "\"" ++ (nameConv i) ++ " = " ++ (embed t) ++ "\""]
-
+-}
+{-
 data Term = TLam IId Term 
           | VLam IId Ty Term
           | VApp Term Term
@@ -189,7 +199,7 @@ data Term = TLam IId Term
 valToTmDecl :: Vdefg -> TmDecl
 valToTmDecl (Nonrec (Vdef ((_,v), _, e))) 
   = TmDecl v (convert e)
-
+-}
 {-
 convert :: Exp -> Term
 convert (Var (_,v)) = TmVar v
@@ -209,9 +219,14 @@ convert (External _ _) = undefined
 
 main = do
   [f] <- getArgs
-  c   <- readFile f
+  c   <- L.readFile f
   let newName = takeBaseName f
+  case parseModule newName c of
+    Left err -> putStrLn $ "Failed: " ++ show err
+    Right  m -> putStrLn $ processModule m newName
+{-
   case parse c 0 of
     FailP s -> putStrLn $ "Failed: " ++ s
     OkP m   -> putStrLn $ processModule m newName
+-}
     --OkP m   -> putStrLn $ header ++ "\n" ++ (embedM $ processModule m)
