059fa4808bb81e668e130d7a211a4dc17e7e630d
Brian Huffman <brianh@cs.pdx.edu>
2011-10-03 12:48:55 +0200

    Halicore_Kind.thy: proof automation for kcont predicate

--- isa/Halicore_Kind.thy
+++ isa/Halicore_Kind.thy
@@ -197,17 +197,47 @@ lemma ty_of_fun_correct:
 unfolding kcont_def ty_of_fun_def
 by (rule theI', erule ex_ex1I, rule fun_has_ty_unique)
 
+lemma ty_of_fun_unique:
+  fixes f :: "'a::kind \<Rightarrow> 'b::kind"
+  shows "fun_has_ty f t \<Longrightarrow> ty_of_fun f = t"
+unfolding ty_of_fun_def
+by (rule the_equality, assumption, erule (1) fun_has_ty_unique)
+
+lemma kcont_intro: "fun_has_ty f t \<Longrightarrow> kcont f"
+  unfolding kcont_def by fast
+
+lemma fun_has_ty_intro:
+  fixes f :: "'a::kind \<Rightarrow> 'b::kind"
+  assumes "has_kind [the_kind TYPE('a)] t (the_kind TYPE('b))"
+  assumes "\<And>x. to_ty (f x) = ty_subst 0 t (to_ty x)"
+  shows "fun_has_ty f t"
+  using assms unfolding fun_has_ty_def by fast
+
 lemma kcont_ty_subst:
   assumes "has_kind [the_kind TYPE('a::kind)] t (the_kind TYPE('b::kind))"
   shows "kcont (\<lambda>x::'a::kind. of_ty (ty_subst 0 t (to_ty x))::'b::kind)"
-unfolding kcont_def
-apply (rule exI)
-apply (simp only: fun_has_ty_def, safe)
+apply (rule kcont_intro)
+apply (rule fun_has_ty_intro)
 apply (rule assms)
 apply (rule of_ty_inverse)
 apply (rule has_kind_ty_subst_0)
 apply (rule assms)
 apply (rule has_kind_to_ty)
+done
+
+lemma kcont_ident: "kcont (\<lambda>x. x)"
+apply (rule kcont_intro [where t="TyVar 0"])
+apply (rule fun_has_ty_intro)
+apply (intro kind_rules)
+apply simp
+done
+
+lemma kcont_const: "kcont (\<lambda>x. c)"
+apply (rule kcont_intro [where t="ty_lift 0 (to_ty c)"])
+apply (rule fun_has_ty_intro)
+apply (rule has_kind_Cons_ty_lift_0)
+apply (rule has_kind_to_ty)
+apply (simp add: ty_subst_ty_lift_cancel)
 done
 
 subsection {* Operations on types *}
@@ -222,6 +252,15 @@ apply (rule has_kind.TyApp)
 apply (subst the_kind_kfun_def [symmetric])
 apply (rule has_kind_to_ty)
 apply (rule has_kind_to_ty)
+done
+
+lemma kcont_Tapp: "\<lbrakk>kcont f; kcont g\<rbrakk> \<Longrightarrow> kcont (\<lambda>x. Tapp (f x) (g x))"
+unfolding kcont_def
+apply (clarify, rename_tac t u)
+apply (rule_tac x="TyApp t u" in exI)
+apply (simp add: fun_has_ty_def the_kind_kfun_def, safe)
+apply (erule (1) has_kind.TyApp)
+apply (simp add: to_ty_Tapp)
 done
 
 definition Tfun :: "(kstar, (kstar, kstar) kfun) kfun"
@@ -247,6 +286,264 @@ apply (simp add: the_kind_kstar_def)
 apply (rule has_kind.TyAll)
 apply (cut_tac ty_of_fun_correct [OF f])
 apply (simp add: fun_has_ty_def the_kind_kstar_def)
+done
+
+subsection {* Parallel substitution and simultaneous continuity *}
+
+primrec tlifts :: "nat \<Rightarrow> ty \<Rightarrow> ty" where
+  "tlifts 0 t = t"
+| "tlifts (Suc i) t = ty_lift i (tlifts i t)"
+
+primrec tsubsts :: "nat \<Rightarrow> ty list \<Rightarrow> ty \<Rightarrow> ty" where
+  "tsubsts i [] t = t"
+| "tsubsts i (x # xs) t = ty_subst i (tsubsts (Suc i) xs t) (tlifts i x)"
+
+definition kconts :: "kind list \<Rightarrow> ((nat \<Rightarrow> ty) \<Rightarrow> 'b::kind) \<Rightarrow> bool"
+  where "kconts ks f \<longleftrightarrow> (\<exists>t. has_kind ks t (the_kind TYPE('b)) \<and>
+    (\<forall>ts. list_all2 (has_kind []) ts ks \<longrightarrow>
+      tsubsts 0 ts t = to_ty (f (\<lambda>n. ts ! n))))"
+
+lemma tlift_0_tlifts: "ty_lift i (tlifts i t) = ty_lift 0 (tlifts i t)"
+by (induct i) (simp_all add: ty_lift_0_ty_lift(1) [symmetric])
+
+lemma ty_substs_tsubsts_Suc_tlifts:
+  "ty_subst 0 (tsubsts (Suc 0) xs t) x =
+    tsubsts 0 xs (foo xs t x)"
+apply (induct xs)
+apply simp defer
+apply simp
+oops
+
+lemma tsubsts_TyApp:
+  "tsubsts i xs (TyApp t1 t2) = TyApp (tsubsts i xs t1) (tsubsts i xs t2)"
+by (induct xs arbitrary: i) simp_all
+
+lemma tsubsts_TyAll:
+  "tsubsts i xs (TyAll k t) = TyAll k (tsubsts (Suc i) xs t)"
+by (induct xs arbitrary: i) (simp_all add: tlift_0_tlifts)
+
+lemma kconts_imp_kcont:
+  fixes f :: "'a::kind \<Rightarrow> 'b::kind"
+  shows "kconts [the_kind TYPE('a)] (\<lambda>s. f (of_ty (s 0))) \<Longrightarrow> kcont f"
+unfolding kconts_def kcont_def
+apply clarify
+apply (rule_tac x=t in exI)
+apply (simp only: fun_has_ty_def, safe)
+apply (drule_tac x="[to_ty x]" in spec)
+apply (simp add: has_kind_to_ty to_ty_inverse)
+done
+
+lemma substVar_lt: "n < i \<Longrightarrow> substVar V i x n = V n"
+unfolding substVar_def by simp
+
+lemma tsubsts_TyVar_lt: "n < i \<Longrightarrow> tsubsts i xs (TyVar n) = TyVar n"
+by (induct xs arbitrary: i) (simp_all add: substVar_lt)
+
+lemma tsubsts_TyVar_nth:
+  "\<lbrakk>i \<le> n; n < i + length xs\<rbrakk>
+   \<Longrightarrow> tsubsts i xs (TyVar n) = tlifts i (xs ! (n - i))"
+apply (induct xs arbitrary: i, simp_all)
+apply (case_tac "i = n")
+apply (simp add: tsubsts_TyVar_lt)
+apply (cases n, simp, simp add: Suc_diff_le ty_subst_ty_lift_cancel)
+done
+
+lemma kconts_bound:
+  assumes "mapsto ks n (the_kind TYPE('a::kind))"
+  shows "kconts ks (\<lambda>s. of_ty (s n)::'a::kind)"
+using assms
+unfolding kconts_def
+apply (rule_tac x="TyVar n" in exI, safe)
+apply (rule has_kind.TyVar [OF assms])
+apply (subst of_ty_inverse)
+apply (clarsimp simp add: list_all2_conv_all_nth mapsto_iff)
+apply (drule spec, drule (1) mp, simp)
+apply (simp add: mapsto_iff list_all2_conv_all_nth)
+apply (simp add: tsubsts_TyVar_nth)
+done
+
+lemma kconts_Tapp:
+  "\<lbrakk>kconts ks f; kconts ks g\<rbrakk> \<Longrightarrow> kconts ks (\<lambda>s. Tapp (f s) (g s))"
+unfolding kconts_def
+apply (clarify, rename_tac t u)
+apply (simp add: the_kind_kfun_def)
+apply (rule_tac x="TyApp t u" in exI, safe)
+apply (erule (1) has_kind.TyApp)
+apply (simp add: tsubsts_TyApp to_ty_Tapp)
+done
+
+lemma Tforall_ty_subst_0:
+  assumes "has_kind [the_kind TYPE('a::kind)] t KStar"
+  shows "Tforall (\<lambda>x::'a::kind. of_ty (ty_subst 0 t (to_ty x))) =
+    of_ty (TyAll (the_kind TYPE('a)) t)"
+unfolding Tforall_def
+apply (subst ty_of_fun_unique [where t=t])
+apply (rule_tac [2] refl)
+apply (simp only: fun_has_ty_def, safe)
+apply (simp only: the_kind_kstar_def assms)
+apply (rule of_ty_inverse)
+apply (simp only: the_kind_kstar_def)
+apply (rule has_kind_ty_subst_0 [OF assms has_kind_to_ty])
+done
+
+lemma shift_eq:
+  "\<Gamma>{i:=k} = (if i \<le> length \<Gamma> then take i \<Gamma> @ [k] @ drop i \<Gamma> else \<Gamma>)"
+apply (induct i arbitrary: \<Gamma>)
+apply (simp add: shift_0)
+apply (case_tac \<Gamma>)
+apply (simp add: shift_Nil_Suc)
+apply (simp add: shift_Cons_Suc)
+done
+
+lemma drop_Suc_shift: "drop (Suc i) (\<Gamma>{i:=k}) = drop i \<Gamma>"
+by (auto simp add: shift_eq min_def)
+
+lemma take_Suc_shift: "i \<le> length \<Gamma> \<Longrightarrow> take (Suc i) (\<Gamma>{i:=k}) = take i \<Gamma> @ [k]"
+by (simp add: shift_eq min_def)
+
+lemma args_lift_conv_map: "args_lift i = map (ty_lift i)"
+by (rule ext, induct_tac x, simp_all)
+
+lemma cons_lift_conv_map: "cons_lift i = map (args_lift i)"
+by (rule ext, induct_tac x, simp_all)
+
+lemma has_kind_imp_ty_lift_cancel:
+  "\<lbrakk>has_kind \<Gamma> t k; length \<Gamma> \<le> i\<rbrakk> \<Longrightarrow> ty_lift i t = t"
+apply (induct arbitrary: i set: has_kind)
+apply (simp add: mapsto_iff skip_def)
+apply simp_all
+apply (simp add: cons_lift_conv_map args_lift_conv_map)
+apply (rule map_idI, rule map_idI, simp add: list_all_iff)
+done
+
+lemma has_kind_tlifts:
+  "has_kind (drop i \<Gamma>) t k \<Longrightarrow> has_kind \<Gamma> (tlifts i t) k"
+apply (induct i arbitrary: \<Gamma>, simp_all add: tlift_0_tlifts)
+apply (case_tac \<Gamma>)
+apply (drule_tac x="[]" in meta_spec, simp)
+apply (simp add: has_kind_imp_ty_lift_cancel)
+apply (simp add: has_kind_Cons_ty_lift_0)
+done
+
+lemma has_kind_tsubsts:
+  assumes "list_all2 (has_kind (drop i \<Gamma>)) xs ks"
+  assumes "has_kind (take i \<Gamma> @ ks @ drop i \<Gamma>) t k"
+  shows "i \<le> length \<Gamma> \<Longrightarrow> has_kind \<Gamma> (tsubsts i xs t) k"
+using assms
+apply (induct xs arbitrary: \<Gamma> ks i)
+apply simp
+apply (simp add: list_all2_Cons1, clarify)
+apply (rule_tac k'="z" in has_kind_ty_subst)
+apply (drule_tac x="\<Gamma>{i:=z}" in meta_spec)
+apply (drule_tac x="zs" in meta_spec)
+apply (drule_tac x="Suc i" in meta_spec)
+apply (drule meta_mp)
+apply (simp add: shift_eq)
+apply (drule meta_mp)
+apply (simp only: drop_Suc_shift)
+apply (drule meta_mp)
+apply (simp only: drop_Suc_shift)
+apply (simp add: take_Suc_shift)
+apply assumption
+apply (erule has_kind_tlifts)
+done
+
+lemma kconts_Tforall:
+  assumes "kconts (the_kind TYPE('a::kind) # ks)
+    (\<lambda>s. f (\<lambda>n. s (Suc n)) (of_ty (s 0)))"
+  shows "kconts ks (\<lambda>s. Tforall (\<lambda>x::'a::kind. f s x))"
+ using assms unfolding kconts_def the_kind_kstar_def
+ apply clarify
+ apply (rule_tac x="TyAll (the_kind TYPE('a)) t" in exI, safe)
+  apply (erule has_kind.TyAll)
+ apply (subgoal_tac "fun_has_ty (f (\<lambda>n. ts ! n)) (tsubsts (Suc 0) ts t)")
+  apply (subst to_ty_Tforall)
+   apply (simp only: kcont_def, erule exI)
+  apply (subst tsubsts_TyAll)
+  apply (rule arg_cong [where f="TyAll (the_kind TYPE('a))"])
+  apply (rule fun_has_ty_unique, assumption)
+  apply (rule ty_of_fun_correct)
+  apply (simp only: kcont_def, erule exI)
+ apply (simp only: fun_has_ty_def, safe)
+  apply (rule has_kind_tsubsts)
+    apply simp
+   apply (simp add: the_kind_kstar_def)
+  apply simp
+ apply (drule_tac x="to_ty x # ts" in spec)
+ apply (drule mp)
+  apply (simp add: has_kind_to_ty)
+ apply (simp add: to_ty_inverse)
+done
+
+lemma mapsto_weaken: "mapsto xs n x \<Longrightarrow> mapsto (xs @ ys) n x"
+by (simp add: mapsto_iff nth_append)
+
+lemma has_kind_weaken: "has_kind \<Gamma> t k \<Longrightarrow> has_kind (\<Gamma> @ \<Gamma>') t k"
+apply (induct set: has_kind)
+apply (auto intro!: has_kind.intros mapsto_weaken
+  elim: list_all_mono [rule_format, COMP swap_prems_rl])
+done
+
+lemma has_kind_Nil_implies: "has_kind [] t k \<Longrightarrow> has_kind \<Gamma> t k"
+using has_kind_weaken [where \<Gamma> = "[]" and \<Gamma>' = \<Gamma>] by simp
+
+lemma args_subst_conv_map:
+  "args_subst i ts t' = map (\<lambda>t. ty_subst i t t') ts"
+by (induct ts, simp_all)
+
+lemma cons_subst_conv_map:
+  "cons_subst i tss t' = map (\<lambda>ts. args_subst i ts t') tss"
+by (induct tss, simp_all)
+
+lemma has_kind_imp_ty_subst_cancel:
+  "\<lbrakk>has_kind \<Gamma> t k; length \<Gamma> \<le> i\<rbrakk> \<Longrightarrow> ty_subst i t x = t"
+apply (induct arbitrary: i x set: has_kind)
+apply (simp add: mapsto_iff substVar_def)
+apply simp_all
+apply (simp add: cons_subst_conv_map args_subst_conv_map)
+apply (rule map_idI, rule map_idI, simp add: list_all_iff)
+done
+
+lemma kconts_const: "kconts ks (\<lambda>s. (a::'a::kind))"
+unfolding kconts_def
+apply (rule_tac x="to_ty a" in exI, safe)
+apply (rule has_kind_Nil_implies)
+apply (rule has_kind_to_ty)
+apply (erule thin_rl)
+apply (rule_tac x="0::nat" in spec)
+apply (induct_tac ts, simp_all)
+apply (rule allI)
+apply (rule has_kind_imp_ty_subst_cancel)
+apply (rule has_kind_to_ty)
+apply simp
+done
+
+lemmas kcont_intros =
+  kconts_imp_kcont
+  kconts_const
+  kconts_bound
+  mapsto_intros
+  kconts_Tapp
+  kconts_Tforall
+
+text "Example of proof automation:"
+
+lemma "kcont (\<lambda>t. Tforall (\<lambda>z. Tapp z (Tforall (\<lambda>u. Tapp (Tapp Tfun t) u))))"
+apply (rule kcont_intros)
+apply (rule kcont_intros)
+apply (rule kcont_intros)
+apply (rule kcont_intros)
+apply (rule kcont_intros)
+apply (rule kcont_intros)
+apply (rule kcont_intros)
+apply (rule kcont_intros)
+apply (rule kcont_intros)
+apply (rule kcont_intros)
+apply (rule kcont_intros)
+apply (rule kcont_intros)
+apply (rule kcont_intros)
+apply (rule kcont_intros)
+apply (rule kcont_intros)
 done
 
 end
